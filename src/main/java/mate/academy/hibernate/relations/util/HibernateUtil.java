2349138671072121abdd39b75426e9974eda128b
anton <anton.surzhok@gmail.com>
2026-07-15 19:13:29 +0300

    Add changes

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,26 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(
+                    "Can't create SessionFactory: " + e.getMessage());
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return sessionFactory;
     }
 }
